1b786bbc9340c12de39c9896fb8f243f9e118993
AluisioNBR <geek.developer27@gmail.com>
2024-11-26 10:21:24 -0300

    wip: intro development

--- src/components/Main/intro.tsx
+++ src/components/Main/intro.tsx
@@ -1,8 +1,8 @@
 export function Intro() {
 	return (
-		<article className="flex flex-col">
-			<p className="text-3xl">Telhados, calhas e construção</p>
-			<h1 className="text-5xl">Especialista a mais de 15 anos</h1>
+		<article className="flex flex-col gap-4 p-40 bg-stone-800 w-full">
+			<p className="text-2xl text-red-500">Telhados, calhas e construção</p>
+			<h1 className="text-5xl font-bold">ESPECIALISTAS<br/>A MAIS DE 15 ANOS</h1>
 		</article>
 	);
 }
