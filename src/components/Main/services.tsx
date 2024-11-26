d5268a0810d918f522ac93e2c828f1ce99684a38
AluisioNBR <geek.developer27@gmail.com>
2024-11-26 11:20:23 -0300

    wip: services development

--- src/components/Main/services.tsx
+++ src/components/Main/services.tsx
@@ -1,26 +1,38 @@
 export function Services() {
 	return (
 		<article
-			className="flex flex-col"
+			className="flex flex-col w-full"
 			id="service"
 		>
-			<ServiceSection title="Construção e manutenção de telhados e calhas">
+			<ServiceSection
+				style="bg-red-700 items-end"
+				title="CONSTRUÇÃO E MANUTENÇÃO DE TELHADOS E CALHAS"
+			>
 				<>
 					<b>O QUE FAZEMOS</b>
+					<br />
 					<br />
 					Construção e reforma de telhados e fabricação, manutenção e instalação
 					de calhas, rufos, pingadeiras e outros
 				</>
 			</ServiceSection>
 
-			<ServiceSection title="Construção civil e reforma em geral">
+			<ServiceSection
+				style="bg-stone-800 items-center"
+				title="CONSTRUÇÃO CIVIL E REFORMA EM GERAL"
+				titleCenter
+			>
 				Construção e reforma de casas e sobrados até 4 andares, acabamentos,
 				pinturas, colocação de pisos e porcelanatos, entre outros
 			</ServiceSection>
 
-			<ServiceSection title="Construção de coberturas e barracões de estruturas metálicas">
+			<ServiceSection
+				style="bg-gray-300 items-start text-black"
+				title="CONSTRUÇÃO DE COBERTURAS E BARRACÕES DE ESTRUTURAS METÁLICAS"
+			>
 				<>
 					<b>O QUE FAZEMOS</b>
+					<br />
 					<br />
 					Reforma e construção da fundação a cobertura de barracões de
 					estruturas metálicas.
@@ -32,14 +44,27 @@ export function Services() {
 
 interface ServiceSectionType {
 	title: string;
+	titleCenter?: boolean;
+	style: string;
 	children: string | JSX.Element | JSX.Element[];
 }
 
-function ServiceSection({ title, children }: ServiceSectionType) {
+function ServiceSection({
+	title,
+	titleCenter,
+	style,
+	children,
+}: ServiceSectionType) {
 	return (
-		<div className="flex flex-col">
-			<h2>{title}</h2>
-			<p>{children}</p>
+		<div className={`flex flex-col gap-4 p-28 w-full ${style}`}>
+			<h2
+				className={`text-3xl font-bold w-2/5 ${
+					titleCenter ? 'text-center' : 'text-left'
+				}`}
+			>
+				{title}
+			</h2>
+			<p className="text-lg w-2/5">{children}</p>
 		</div>
 	);
 }
