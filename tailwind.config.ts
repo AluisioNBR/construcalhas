ab2d5e21e824b5a58b26fc6e98d0a071da091b91
AluisioNBR <geek.developer27@gmail.com>
2024-11-26 01:13:54 -0300

    chore: keyframe hoverLink bug fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      keyframes: {
-      	hoverLink: {
-      		from: { opacity: 1 },
-      		to: { opacity: 0.5 }
-      	}
-      },
-      animation: {
-      	hoverLink: "hoverLink 1s easy-in-out"
-      },
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			keyframes: {
+				hoverLink: {
+					from: { opacity: '1' },
+					to: { opacity: '0.5' },
+				},
+			},
+			animation: {
+				hoverLink: 'hoverLink 1s easy-in-out',
+			},
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
