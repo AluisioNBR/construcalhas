930a03ba0f1641b4f71ccdf55b13e3fbaff6bf72
AluisioNBR <geek.developer27@gmail.com>
2024-11-30 01:08:50 -0300

    feat: intro padding fixed

--- src/components/Main/intro.tsx
+++ src/components/Main/intro.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export function Intro() {
 	return (
-		<article className="flex flex-col gap-4 md:p-[10.5rem] px-16 py-32 bg-blend-overlay bg-cover bg-no-repeat bg-left bg-[url('/images/telhado2-1.jpg')] bg-stone-800 w-full">
+		<article className="flex flex-col gap-4 md:p-[10.5rem] px-12 py-32 bg-blend-overlay bg-cover bg-no-repeat bg-left bg-[url('/images/telhado2-1.jpg')] bg-stone-800 w-full">
 			<div className="flex flex-row items-center gap-2">
 				<Image src="/images/icon.png" alt="logo" width={32} height={32} />
 				<p className="md:text-2xl text-xl text-blue-500">Calhas e telhados</p>
